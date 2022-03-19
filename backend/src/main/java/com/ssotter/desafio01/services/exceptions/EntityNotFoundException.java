cf82b849483340b730a0e5a754e7910c4a6e4a41
Sergio Llopart Sotter <sergio.sotter@network-rg.com.br>
2022-03-19 12:06:04 -0300

    Find Client by id, exception handling

---
+++ backend/src/main/java/com/ssotter/desafio01/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ssotter.desafio01.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
